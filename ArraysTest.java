8e1eaa07fd7aebcd08b621afc039a74cb14d7480
Sofia Egan <sofia@eerier.dev>
2026-05-06 21:22:47 +0000

    adding array stuff

--- ArraysTest.java
+++ ArraysTest.java
@@ -1,11 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArraysTest {
     public static ArrayList<String> arrayList = new ArrayList<String>();
-    public static String[][] 2dArray = new int[2][5];
+    public static int[][][] array = new int[4][2][9];
 
     public static void main(String args[]) {
 
-        System.out.println(2dArray);
+        array[3][1][7] = 2;
 
+        System.out.println(Arrays.deepToString(array));
+
+        array
     }
     
 }
+//[],[]
+// [0,0]
+// [0,0,0]
+//  [
+//    [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+//    [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+//    [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+//    [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,2,0]]
+// ]
