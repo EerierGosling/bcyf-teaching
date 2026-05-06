fd8c86ccbfbcd6b1e736bd280d8a57adb1aebf75
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2026-05-06 16:50:28 -0400

    adding array stuff

--- ArraysTest.java
+++ ArraysTest.java
@@ -1,3 +1,11 @@
 public class ArraysTest {
+    public static ArrayList<String> arrayList = new ArrayList<String>();
+    public static String[][] 2dArray = new int[2][5];
+
+    public static void main(String args[]) {
+
+        System.out.println(2dArray);
+
+    }
     
 }
